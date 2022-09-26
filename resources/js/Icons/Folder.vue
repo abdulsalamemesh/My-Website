7b2c14c850aea54ed7bc5209c1e6aef304febb31
Abdulsalam Emesh <abdulsalam.emesh.work@gmail.com>
2022-09-26 08:14:45 +0200

    add source

--- resources/js/Icons/Folder.vue
+++ resources/js/Icons/Folder.vue
@@ -1,7 +1,8 @@
 <template>
+    <!--https://github.com/mallowigi/a-file-icon-idea/blob/master/common/src/main/resources/icons/plugins/ruby/rails/projectView/minor_folder_closed.svg-->
     <svg big="true" xmlns="http://www.w3.org/2000/svg" version="1.1" width="16px" height="16px" viewBox="0 0 24 24" class="h-5 w-5">
         <path fill="#607d8b"
-              d="M10,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V8C22,6.89 21.1,6 20,6H12L10,4Z" />
+              d="M10,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V8C22,6.89 21.1,6 20,6H12L10,4Z"/>
     </svg>
 </template>
 
