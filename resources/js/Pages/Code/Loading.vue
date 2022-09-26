6b4df79200ef4704c9c91c511f5c46ccd67412f0
Abdulsalam Emesh <abdulsalam.emesh.work@gmail.com>
2022-09-26 07:55:25 +0200

    add page loading

---
+++ resources/js/Pages/Code/Loading.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import {onMounted, ref} from "vue";
+import ajax from "lodash";
+import {Inertia} from "@inertiajs/inertia";
+let count = ref(0);
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+onMounted(() =>{
+
+    delay(5000).then(() => { Inertia.get(route('index')) });
+})
+</script>
+
+<template>
+
+    <div class="h-screen w-screen bg-[#3c3f41] flex justify-center items-center">
+        <div class="max-w-xl">
+            <img src="/phpstorm.PNG" alt="phpstrom" class="object-center mb-2">
+            <p class="text-center text-[#a9b1b3]">Loading your IDE</p>
+        </div>
+    </div>
+</template>
