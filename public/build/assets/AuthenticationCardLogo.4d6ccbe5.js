ad2c3b44cda477dac5e81cc2e416571a27361b1b
Abdulsalam Emesh <abdulsalam.emesh.work@gmail.com>
2024-11-02 00:08:06 +0100

    build assets

--- public/build/assets/AuthenticationCardLogo.4d6ccbe5.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,w as a,g as s,L as o,a as c}from"./app.e573c44e.js";const r=c("p",{class:"text-white text-3xl"},"Abdulsalam Emesh",-1),l={__name:"AuthenticationCardLogo",setup(_){return(n,i)=>(e(),t(s(o),{href:"/"},{default:a(()=>[r]),_:1}))}};export{l as _};
